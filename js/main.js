9cc4e526fb98c0ba6c5375c67e33dcc4379dc5d6
esmael-ahmed <esmaelahmed045@gmail.com>
2023-08-10 19:18:26 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+
+let users = [];
+
+if (localStorage.getItem("usersList") != null)
+{
+    users = JSON.parse(localStorage.getItem("usersList"));
+}
+let inEmail = document.getElementById('signinEmail');
+let inPassword = document.getElementById('signinPass');
+let inMass = document.getElementById('signinMassage');
+
+// console.log(hText.innerText);
+
+document.getElementById('login').addEventListener('click',function(e) {
+    for (let i = 0; i < users.length; i++)
+    {
+        if (users[i].userEmail == inEmail.value)
+        {
+            if (users[i].userPassword == inPassword.value)
+            {
+                // window.open('hello.html');
+                // document.getElementById('helloText').innerHTML = `hello ${users[i].uName}`;
+                inMass.innerHTML = '';
+                window.location.href = `hello.html?username=${users[i].userName}`;
+                // console.log(`hello ${users[i].userName}`);
+            }
+        }
+        else
+        {
+            inMass.innerHTML = 'incorrect email or password';
+        }
+    }
+});
+
+console.log(users);
+
+
---
+++ js/signup.js
@@ -0,0 +1,37 @@
+
+// let users = [{uName: 'esmael-ahmed', uEmail: 'esmaelahmed045@gmail.com', uPassword: '2991995@Login'}];
+let users = [];
+
+if (localStorage.getItem("usersList") != null)
+{
+    users = JSON.parse(localStorage.getItem("usersList"));
+}
+
+
+let upName = document.getElementById('signupName');
+let upEmail = document.getElementById('signupEmail');
+let upPassword = document.getElementById('signupPass');
+let upMass = document.getElementById('signupMassage'); 
+
+
+document.getElementById("signup").addEventListener('click', function(e) {
+for (let i = 0; i <users.length; i++) {
+    if (users[i].userEmail == upEmail.value)
+    {
+        upMass.innerHTML = "the email is already registered";
+        return;
+    }
+}
+
+let user = {userName: upName.value, userEmail: upEmail.value, userPassword: upPassword.value}
+users.push(user);
+localStorage.setItem("usersList", JSON.stringify(users));
+upMass.classList.replace('text-danger', 'text-success');
+upMass.innerHTML = "Successfully registered";
+setTimeout(function () {
+    window.location.href = `signin.html`;
+}, 2000);
+
+});
+
+console.log(users);
